5fd326a43075006e36ba2c84d2e70bff96241262
Pablo Mendoza <147556543+MendozaCal@users.noreply.github.com>
2024-05-20 19:02:45 -0500

    Move Player

---
+++ Assets/Player/MoveController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+    [SerializeField] protected float maxSpeed = 15;
+    protected float distanceAtack = 20;
+    protected Transform Player;
+    protected Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        Player = GameObject.FindWithTag("Player").transform;
+    }
+    void Update()
+    {
+        Move();
+    }
+    protected virtual void Move()
+    {
+
+    }
+}
---
+++ Assets/Player/MovePlayer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MoveController
+{
+    [SerializeField] protected float rotationSpeed;
+    protected override void Move()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalInput, 0, verticalInput) * maxSpeed * Time.deltaTime;
+        Quaternion rotation = Quaternion.Euler(0, horizontalInput * rotationSpeed * Time.deltaTime, 0);
+
+        rb.MovePosition(transform.position + transform.TransformDirection(movement));
+        rb.MoveRotation(rb.rotation * rotation);
+    }
+
+}
